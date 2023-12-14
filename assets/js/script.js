da35f70d949f44bfd014d55b3c32d7ea3e8b00a5
Allwrightben <ben.allwright@learningpeople.co.uk>
2023-12-14 12:03:34 -0600

    Add JS for new HTML structure and list out the functions we will need

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Hi! I'm working! :)")
+let startButton = document.getElementById('start-btn')
+let questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('start the bloody game')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer () {
+
+}
+
